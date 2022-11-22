da85c38f5292078d6bbfafe00da27cef587d83c7
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2022-11-21 21:03:28 -0500

    se crean activities landing y profile

---
+++ app/src/main/java/com/example/inerzia/ProfileActivity.java
@@ -0,0 +1,21 @@
+package com.example.inerzia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_profile,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
