6092456a34c10960bf8fe7c0c8a1c1ab1cd5b0e4
CamilaUnab <camila.bautista@o365.unab.edu.co>
2022-11-26 16:44:55 -0500

    Actualizacion activity Home y Practice

--- app/src/main/java/com/example/inerzia/practice.java
+++ app/src/main/java/com/example/inerzia/practice.java
@@ -1,14 +1,14 @@
-package com.example.inerzia;
+package com.inerzia.inerzia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class practice extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_practice);
     }
 }
