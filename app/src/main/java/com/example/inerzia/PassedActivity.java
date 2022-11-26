6bf9052ffe91ac298d62af81181d35059cfd8adb
tiernito98 <juan.sanchez3@o365.unab.edu.co>
2022-11-26 17:03:48 -0500

    Actualizacion t98

---
+++ app/src/main/java/com/example/inerzia/PassedActivity.java
@@ -0,0 +1,14 @@
+package com.example.inerzia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passed);
+    }
+}
