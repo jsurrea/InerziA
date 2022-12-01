535276813ece783058fe45ad02f8b9775d12c3de
CamilaUnab <camila.bautista@o365.unab.edu.co>
2022-11-30 20:51:54 -0500

    Actualizacion activity

---
+++ app/src/main/java/com/example/inerzia/Practice1Activity.java
@@ -0,0 +1,14 @@
+package com.example.inerzia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Practice1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice1);
+    }
+}
