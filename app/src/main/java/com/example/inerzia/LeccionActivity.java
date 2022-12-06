308c5b38486560570266a852a901805e5b362a67
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2022-12-06 17:10:12 -0500

    navegacion activity lection

--- app/src/main/java/com/example/inerzia/LeccionActivity.java
+++ app/src/main/java/com/example/inerzia/LeccionActivity.java
@@ -2,7 +2,9 @@ package com.example.inerzia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LeccionActivity extends AppCompatActivity {
 
@@ -11,4 +13,18 @@ public class LeccionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leccion);
     }
+
+    public void home(View view){
+        Intent comeback = new Intent(LeccionActivity.this, HomeActivity.class);
+        startActivity(comeback);
+    }
+
+    public void perfil(View view){
+        Intent comeback = new Intent(LeccionActivity.this, ProfileActivity.class);
+        startActivity(comeback);
+    }
+
+
+
+
 }
