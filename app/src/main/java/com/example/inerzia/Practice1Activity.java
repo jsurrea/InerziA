65bdbe8407e1efa23e26651288589721639e60c5
IFIX404 <williamandres321@gmail.com>
2022-12-06 15:50:28 -0500

    navegabilidad

--- app/src/main/java/com/example/inerzia/Practice1Activity.java
+++ app/src/main/java/com/example/inerzia/Practice1Activity.java
@@ -2,13 +2,26 @@ package com.example.inerzia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Practice1Activity extends AppCompatActivity {
 
+    Button btnU1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice1);
+        btnU1 = findViewById(R.id.button);
+
+        btnU1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Practice1Activity.this,LeccionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
