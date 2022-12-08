735f946cee7f5ee3acd93574a4f3d51470b48e89
IFIX404 <williamandres321@gmail.com>
2022-12-08 16:27:29 -0500

    cambios en los modelos

--- app/src/main/java/com/example/inerzia/modelo/Leccion.java
+++ app/src/main/java/com/example/inerzia/modelo/Leccion.java
@@ -3,19 +3,20 @@ package com.example.inerzia.modelo;
 public class Leccion {
 
   //Declaracion de variables
-
   private String keyword;
   private String audio;
   private String imagen;
-  private String phrase;
+  private String opcion1;
+  private String opcion2;
+  private String opcion3;
+  private String opcion4;
 
   //Constructor
-  public Leccion (){
+  public Leccion(){
 
   }
 
   //Encapsulacion
-
 
   public String getKeyword() {
     return keyword;
@@ -41,23 +42,50 @@ public class Leccion {
     this.imagen = imagen;
   }
 
-  public String getPhrase() {
-    return phrase;
+  public String getOpcion1() {
+    return opcion1;
   }
 
-  public void setPhrase(String phrase) {
-    this.phrase = phrase;
+  public void setOpcion1(String opcion1) {
+    this.opcion1 = opcion1;
+  }
+
+  public String getOpcion2() {
+    return opcion2;
+  }
+
+  public void setOpcion2(String opcion2) {
+    this.opcion2 = opcion2;
+  }
+
+  public String getOpcion3() {
+    return opcion3;
+  }
+
+  public void setOpcion3(String opcion3) {
+    this.opcion3 = opcion3;
+  }
+
+  public String getOpcion4() {
+    return opcion4;
+  }
+
+  public void setOpcion4(String opcion4) {
+    this.opcion4 = opcion4;
   }
 
   //Metodo toString
 
   @Override
   public String toString() {
-    return "Leccion{" +
+    return "Test{" +
             "keyword='" + keyword + '\'' +
             ", audio='" + audio + '\'' +
             ", imagen='" + imagen + '\'' +
-            ", phrase='" + phrase + '\'' +
+            ", opcion1='" + opcion1 + '\'' +
+            ", opcion2='" + opcion2 + '\'' +
+            ", opcion3='" + opcion3 + '\'' +
+            ", opcion4='" + opcion4 + '\'' +
             '}';
   }
 }
